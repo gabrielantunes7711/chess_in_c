d7b6bd0d9c3687c3594f37174071f64bbb8dddef
Gabriel Antunes <gabrielantunes7711@gmail.com>
2025-03-09 07:28:18 -0300

    Feat: Adiciona funções e recursividade

--- chess_movies.c
+++ chess_movies.c
@@ -1,34 +1,55 @@
 #include <stdio.h>
 
+void tower_move(int houses){
+  if (houses > 0) {
+    printf("Direita\n");
+
+    tower_move(houses - 1);
+  }
+}
+
+void bishop_move(int houses){
+  for (int i = houses; i > 0; i--){
+    printf("Cima ");
+
+    for (int j = 0; j < 1; j++){
+      printf("Direita\n");
+    }
+  }
+}
+
+void queen_move(int houses){
+  if (houses > 0) {
+    printf("Esquerda\n");
+
+    queen_move(houses - 1);
+  }
+}
+
+void horse_move(int houses) {
+  for (int i = houses; i > 1; i--)  {
+    printf("Cima\n");
+
+    for (int j = 2; j == i; j++) {
+      printf("Direita\n");
+    }
+  }
+}
+
 int main(){
   int tower_movies = 5, bishop_movies = 5, queen_movies = 8, horse_movies = 3;
 
-  printf("Movimentação do cavalo: 2 casas à baixo e à esquerda\n");
-  for (horse_movies; horse_movies > 0; horse_movies--)  {
-    printf("Baixo\n");
+  printf("Movimentacao do cavalo: 2 casas a cima e a direita\n");
+  horse_move(horse_movies);
 
-    while (horse_movies == 2) {
-      printf("Esquerda\n");
-      horse_movies--;
-    }
-  }
+  printf("\nMovimentacao da torre: 5 casas a direita\n");
+  tower_move(tower_movies);
 
-  printf("\nMovimentação da torre: 5 casas à direita\n");
-  for (tower_movies; tower_movies > 0; tower_movies--)  {
-    printf("Direita\n");
-  }
+  printf("\nMovimentacao do bispo: 5 casas na diagonal para cima e a direita\n");
+  bishop_move(bishop_movies);
 
-  printf("\nMovimentação do bispo: 5 casas na diagonal para cima e à direita\n");
-  while (bishop_movies > 0) {
-    printf("Cima Direita\n");
-    bishop_movies--;
-  }
-
-  printf("\nMovimentação da rainha: 8 casas à esquerda\n");
-  do {
-    printf("Esquerda\n");
-    queen_movies--;
-  } while (queen_movies > 0);
+  printf("\nMovimentacao da rainha: 8 casas a esquerda\n");
+  queen_move(queen_movies);
   
   return 0;
 }
